502d8614946625cf55e8142ed74214fa095c8b4b
Carita Carstensen <carita.carstensen@gmail.com>
2021-11-04 18:23:24 +0200

    Removing mails and updating folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'details',
     loadChildren: () => import('./pages/details/details.module').then( m => m.DetailsPageModule)
+  },  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
+
 
 ];
  
