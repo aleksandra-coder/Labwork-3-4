ffcc5919522aca24c428924ef5bcd5a91e91f63d
Carita Carstensen <carita.carstensen@gmail.com>
2021-11-04 18:32:55 +0200

    Revert "fixing"
    This reverts commit 23b1b33a559f562f6cbcd29d1d9b8da9d8bd4815.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,8 +18,7 @@ const routes: Routes = [
   {
     path: 'details',
     loadChildren: () => import('./pages/details/details.module').then( m => m.DetailsPageModule)
-  },
-  {
+  },  {
     path: 'tabs',
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
